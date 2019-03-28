25819c3ff23902c16cdd89b2f8e82a0014dc1cfb
WMXPY <wm@mengw.io>
2019-03-28 15:27:31 -0500

    consume coco

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,13 @@
  */
 
 import { Environment } from "@barksh/core";
-import * as Program from "commander";
+import { Coco } from "@sudoo/coco";
 import { getEnvironment } from "./environment";
 
 export const BarkCli = (args: string[]) => {
 
     const env: Environment = getEnvironment();
-
-    Program.version('0.0.1');
-
-    Program
-        .command('env', 'Print environment information')
-        .action(() => {
-            console.log(env);
-        });
-
-    Program
-        .command('ping', 'Pong')
-        .action(() => {
-            console.log('Pong');
-        });
-
-    Program.parse(args);
+    const coco: Coco = Coco.create();
 };
 
 export default BarkCli;
