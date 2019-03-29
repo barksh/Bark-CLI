359a6d7867411fed202fb37babcd61171b5ddc84
WMXPY <wm@mengw.io>
2019-03-29 09:31:19 -0500

    awesome coco

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,20 @@
  */
 
 import { Environment } from "@barksh/core";
-import { Coco } from "@sudoo/coco";
+import { Argument, Coco, Command, Option } from "@sudoo/coco";
 import { getEnvironment } from "./environment";
 
 export const BarkCli = (args: string[]) => {
 
     const env: Environment = getEnvironment();
     const coco: Coco = Coco.create();
+
+    coco.command(Command.create('test')
+        .argument(Argument.create('hello'))
+        .option(Option.create('a').setName('name'))
+        .then((inputs) => console.log(inputs)));
+
+    coco.go(args);
 };
 
 export default BarkCli;
