f125c1fcd6f0b00e94dffbf192f29755f0d38c9e
WMXPY <wm@mengw.io>
2019-04-12 17:14:34 -0500

    make update finally works

--- src/update.ts
+++ src/update.ts
@@ -4,14 +4,16 @@
  * @description Update
  */
 
-import { Core } from "@barksh/core";
+import { Core, Environment } from "@barksh/core";
 import { Command } from "@sudoo/coco";
+import { replaceConfig } from "./environment";
 
 export const createUpdateCommand = (core: Core): Command => {
 
     return Command.create('update')
         .then(async (inputs: Record<string, string>) => {
 
-            console.log(await core.updateAllSources());
+            const newEnv: Environment = await core.updateAllSources();
+            await replaceConfig(newEnv);
         });
 };
