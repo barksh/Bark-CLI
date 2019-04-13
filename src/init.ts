7109568f12f77234ccdc9ada1b9f8d4de8bb1edc
WMXPY <wm@mengw.io>
2019-04-13 14:57:06 -0500

    use type definiation for init then listener

--- src/init.ts
+++ src/init.ts
@@ -11,9 +11,13 @@ export const createInitCommand = (core: Core): Command => {
 
     return Command.create('init')
         .argument(Argument.create('template'))
-        .then(async (inputs: Record<string, string>) => {
+        .argument(Argument.create('target'))
+        .then(async (inputs: {
+            readonly template: string;
+            readonly target: string;
+        }) => {
 
-            console.log(inputs);
+            console.log(inputs.template, inputs.target);
 
             console.log(await core.attemptFindTemplate(inputs.template));
         });
